8a6c27f32bd4ac06b4ca6cdfcb1640b860eafa78
Mikayla Randall <mikayla.mae@icloud.com>
2024-03-08 12:20:28 -0700

    the big stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function Welcome() {
+  return <h1>College Basketball Teams</h1>;
+}
+
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+    return (
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h3>Mascot: {oneTeam.name}:</h3>
+        <h3>
+          Loaction: {oneTeam.city}, {oneTeam.state}
+        </h3>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {CollegeBasketballTeams.teams.map((oneTeam, index) => (
+        <React.Fragment key={index}>
+          <Team {...oneTeam} />
+          {index !== CollegeBasketballTeams.teams.length - 1 && <hr />}
+        </React.Fragment>
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
